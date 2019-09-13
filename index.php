8762521e90f796b82ec5a49a0b7666e02a601326
Andrey Filippov <afi@i-loto.ru>
2019-09-13 09:29:08 +0300

    added bash scripts

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+
+
+var_dump($_SERVER);
+var_dump($_ENV);
---
+++ start-web-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker-compose up --build
+
+# docker-compose run --rm php7 php -S 0.0.0.0:9292 -t /app
+
+# docker-compose run --rm dizel-app-php73 bash -c 'export APP_CONFIG=config/web.dev.php APP_DEBUG_MODE=true && php -S 0.0.0.0:9191 -t /app'
+
+# IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' php7) && echo http://$IP:9292
